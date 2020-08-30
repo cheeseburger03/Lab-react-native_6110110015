0534155fa47786d1635bc6549a7214553a80d426
cheeseburger03 <60773307+cheeseburger03@users.noreply.github.com>
2020-08-30 15:33:38 +0700

    Session 5

--- components/Weather.js
+++ components/Weather.js
@@ -1,14 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 import {Text, ImageBackground,StyleSheet , View} from 'react-native'
 import Forecast from './Forecast'
 
 export default function Weather(props){
-    const [forecastInfo, setForecastInfo] = useState({
-            main: 'Main',
-            description: 'Discription',
+        const [forecastInfo, setForecastInfo] = useState({
+            main: 'Main Area',
+            description: 'Description Area',
             temp: 0
-    })
+            })
 
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=23310f2894e3255687adace8ca2e16d5 `)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp });
+            })
+        .catch((error) => {
+                console.warn(error);
+        });
+    }
+}, [props.zipCode])
+       
+    
     return(
         <ImageBackground source={require('../sky.jpg')} style={styles.backdrop}>
             <View style = {text.backdrop}>
